2fca9e477adaf0ec0f35fd966d1ecda59b8a28c8
SPRUHA2008 <kurlespruha@gmail.com>
2026-02-05 10:58:30 +0530

    Add recursive power function in q3.cpp

---
+++ exp3/q3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int power(int n, int m) {
+    if (m == 0)
+        return 1;
+    return n * power(n, m - 1);
+}
+
+int main() {
+    int N, m;
+    cout << "Enter base (N): ";
+    cin >> N;
+    cout << "Enter power (m): ";
+    cin >> m;
+
+    cout << "Result: " << power(N, m);
+    return 0;
+}
