fb100eb848dc4d20b3a4209564f11027306cbb2f
SPRUHA2008 <kurlespruha@gmail.com>
2026-02-07 17:46:06 +0530

    Create q6.cpp

---
+++ exp4/q6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<vector<int>> a = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    for (int i = 0; i < a.size(); i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < a[i].size(); j++)
+                cout << a[i][j] << " ";
+        } else {
+            for (int j = a[i].size() - 1; j >= 0; j--)
+                cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
